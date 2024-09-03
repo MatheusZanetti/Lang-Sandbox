345a7481bcd274b022d7a80e53647cfa57aec936
MatheusZanetti <149878042+MatheusZanetti@users.noreply.github.com>
2024-09-03 20:38:36 -0300

    Add files via upload

---
+++ CommonLisp/OperacoesBasicas.lisp
@@ -0,0 +1,44 @@
+;Testando notações, operadores básicos 
+
+#|Soma sem var |#
+(defun SomaNoVar()
+    (format T "Isso é uma soma de 8 + 10 SEM VARIAVEL ~%")
+    (format T "O Resultado é: ~a~%" (+ 8 10)))
+
+;;Subtracao sem var
+(defun SubNoVar()
+    (format T "Isso é uma Subtração de 8 - 10 SEM VARIAVEL ~%")
+    (format T "O resultado é: ~a~%" (- 8 10)))
+
+#|Multiplacao sem var |#
+(defun MultiNoVar()
+    (format T "Isso é uma multiplicação de 3 * 6 SEM VARIAVEL ~%")
+    (format T "O resultado é: ~a~%" (* 3 6)))
+
+;;Divisao Numero real sem Var
+(defun DivNoVar()
+    (format T "Isso é uma divisão real de 3/6 SEM VARIAVEL ~%")
+    (format T "O resultado é: ~a~%" (/ 3 6.0)))
+
+#|Divisao numero inteiro sem var |#
+(defun DivIntNoVar()
+    (format T "Isso é uma divisao inteira de 3/6 SEM VARIAVEL ~%")
+    (format T "O Resultado é: ~a~%" (/ 3 6 )))
+
+;;Testando SQRT
+(defun Raiz()
+    (format T "Isso é uma raiz quadrada de 25 (SQRT 25) SEM VARIAVEL ~%")
+    (format T "O Resultado é: ~a~%" (SQRT 25)))
+
+#|Testando Absolute value|#
+(defun ValAbsoluto()
+    (format T "Isso é o valor absoluto de um numero (ABS -3) SEM VARIAVEl ~%")
+    (format T "O resutaldo é: ~a~%" (ABS -3)))
+
+(SomaNoVar)
+(SubNoVar)
+(MultiNoVar)
+(DivNoVar)
+(DivIntNoVar)
+(Raiz)
+(ValAbsoluto)
